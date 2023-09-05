f93bb8f32bfa19c777a34aafdb8c79e1fc173b69
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2023-09-05 13:11:12 -0500

    added more external invariants

--- test/echidna/property/AppManager.e.sol
+++ test/echidna/property/AppManager.e.sol
@@ -22,4 +22,80 @@ contract TestAppManager is TestCommonEchidna {
         if (applicationAppManager.isAppAdministrator(address(this))) return true;
         return false;
     }
+
+    function echidna_appAdminNotZero() public returns(bool){
+        if (!applicationAppManager.isAppAdministrator(address(0))) return true;
+        return false;
+    }
+
+    function echidna_riskAdminNotZero() public returns(bool){
+        if (!applicationAppManager.isRiskAdmin(address(0))) return true;
+        return false;
+    }
+
+    function echidna_accessLevelAdminNotZero() public returns(bool){
+        if (!applicationAppManager.isAccessTier(address(0))) return true;
+        return false;
+    }
+
+
+    function echidna_ruleAdminNotZero() public returns(bool){
+        if (!applicationAppManager.isRuleAdministrator(address(0))) return true;
+        return false;
+    }
+
+    function echidna_zeroAddressNoAccessLevel() public returns(bool){
+        if (applicationAppManager.getAccessLevel(address(0))==0) return true;
+        return false;
+    }
+
+    function echidna_zeroAddressNoRiskScore() public returns(bool){
+        if (applicationAppManager.getRiskScore(address(0))==0) return true;
+        return false;
+    }
+
+
+    function echidna_zeroAddressNoGeneralTags() public returns(bool){
+        if (applicationAppManager.getAllTags(address(0)).length==0) return true;
+        return false;
+    }
+
+    function echidna_zeroAddressNoTreasury() public returns(bool){
+        if (!applicationAppManager.isTreasury(address(0)))return true;
+        return false;
+    }
+
+   
+
+    /* ------------ Provider Addresses -------------------*/
+
+    function echidna_generalTagProviderNotZero() public returns(bool){
+        if (applicationAppManager.getGeneralTagProvider()!=address(0))return true;
+        return false;
+    }
+
+    function echidna_accessLevelProviderNotZero() public returns(bool){
+        if (applicationAppManager.getAccessLevelProvider()!=address(0))return true;
+        return false;
+    }
+
+    function echidna_riskScoreProviderNotZero() public returns(bool){
+        if (applicationAppManager.getRiskScoresProvider()!=address(0))return true;
+        return false;
+    }
+
+    function echidna_accountsProviderNotZero() public returns(bool){
+        if (applicationAppManager.getAccountProvider()!=address(0))return true;
+        return false;
+    }
+
+    function echidna_pauseRulesProviderNotZero() public returns(bool){
+        if (applicationAppManager.getPauseRulesProvider()!=address(0))return true;
+        return false;
+    }
+
+
+
+   
+
 }
