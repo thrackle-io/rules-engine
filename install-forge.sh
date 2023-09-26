dc265db0d12891eee19231d36b423c66692256f2
Robert Kotz <robertkotz@Roberts-MBP.home>
2023-09-25 20:14:19 -1000

    newest python requires using a venv, and seems we might need to upgrade the eth-abi dependency for it to work.... testing

--- install-forge.sh
+++ install-forge.sh
@@ -2,6 +2,12 @@
 
 source ~/.bashrc
 foundryup
-pip install -r requirements.txt
-git submodule update --init --recursive
-#make deployAll deployAllApp
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+
+## This line fails because the git repo isn't actually on the container, just a copy of all the code... 
+## Do we need this??
+#git submodule update --init --recursive   
+
+make deployAll deployAllApp
