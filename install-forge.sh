0aba028b5c79051cf0d86c6d733441e4291cfdf5
Robert Kotz <robertkotz@Roberts-MBP.home>
2023-10-18 13:04:08 -1000

    trying to fix the tron build...

--- install-forge.sh
+++ install-forge.sh
@@ -6,8 +6,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
-## This line fails because the git repo isn't actually on the container, just a copy of all the code... 
-## Do we need this??
-#git submodule update --init --recursive   
+git submodule update --init --recursive   
 
-make deployAll deployAllApp
+make build deployAll deployAllApp
