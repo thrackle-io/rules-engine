9f95468a8591bc52f548bf302c5899e5cd8bb931
Oscar Serna <oscar.eduardo.serna@gmail.com>
2023-11-24 04:34:58 -0600

    encapsulated some utility functions

--- test/helpers/Utils.sol
+++ test/helpers/Utils.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.17;
 contract Utils{
 
     /**
+    * @dev calculates the difference between 2 uints without risk of overflow/underflow
+    * @param x uint
+    * @param y uint
+    * @return diff the absolute difference between *x* and *y*
+    */
+    function safeDiff(uint x, uint y) public pure returns (uint diff){
+        diff = x > y ? x - y : y - x;
+    }
+
+    /**
     * @dev gets a bytes variable and checks if it is an ascii value or not.
     * @notice this algorythm is 100% accurate in the negative case, but false
     * positives are possible. This is because if all the bytes are between 30
@@ -24,16 +34,39 @@ contract Utils{
         return isAscii;
     }
 
-    function decodeHexDecimalBytes(bytes memory byteDecimal) public pure returns(uint256 decodedUint){
-        for (uint i; i < byteDecimal.length; i++){
-            //emit Log(bytes(byteDecimal[i]));
-            uint tens = ((uint(uint8(byteDecimal[i])) / 16 ) * 10);
-            uint units = ( uint(uint8(byteDecimal[i])) - ((uint(uint8(byteDecimal[i])) / 16 ) * 16));
-            if (i != byteDecimal.length - 1) decodedUint += ( tens + units) * (10 ** ((byteDecimal.length - i - 1 )*2));
+    /**
+    * @dev decodes a byte variable trying to express a decimal number. For instance 0x297462 = 297462.
+    * @param bytesDecimal the byte variable to convert to uint.
+    * @return decodedUint the uint that the bytes variable was trying to imply.
+    */
+    function decodeHexDecimalBytes(bytes memory bytesDecimal) public pure returns(uint256 decodedUint){
+        for (uint i; i < bytesDecimal.length; i++){
+            uint tens = ((uint(uint8(bytesDecimal[i])) / 16 ) * 10);
+            uint units = ( uint(uint8(bytesDecimal[i])) - ((uint(uint8(bytesDecimal[i])) / 16 ) * 16));
+            if (i != bytesDecimal.length - 1) decodedUint += ( tens + units) * (10 ** ((bytesDecimal.length - i - 1 )*2));
             else decodedUint += ( tens + units);
         }
     }
 
+    /**
+    * @dev decodes an ascii number to return the uint
+    * @param ascii the number to convert to uint
+    * @return decodedUint 
+    */
+    function decodeAsciiUint(bytes memory ascii) public pure returns(uint256 decodedUint){
+        for (uint i; i < ascii.length; i++){
+            uint units = uint(uint8(ascii[i])) - 0x30;
+            if (i != ascii.length - 1) decodedUint += units * (10 ** ((ascii.length - i - 1 )));
+            else decodedUint +=  units;
+        }
+    }
+
+    /**
+    * @dev checks if 2 bytes variables are identical
+    * @param x first bytes variable to compare
+    * @param y second bytes variable to compare 
+    * @return true if they x and y are identical 
+    */
     function areBytesEqual(bytes memory x, bytes memory y) public pure returns(bool) {
         return keccak256(x) == keccak256(y);
     }
--- test/liquidity/ProtocolNFTAMMCalcFuzz.t.sol
+++ test/liquidity/ProtocolNFTAMMCalcFuzz.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+/// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
@@ -31,18 +31,17 @@ contract ProtocolNFTAMMFactoryFuzzTest is TestCommonFoundry, Utils {
     event IsAscii(bool);
     event Passed();
 
-    //error OutOfTolerance(bytes _contract, bytes _python);
-    error OutOfTolerance(uint _contract, uint _python);
+    error OutOfTolerance(uint _contract, uint _python); //700e344e
 
     using Strings for uint256;
     uint256 constant PRECISION_DECIMALS = 8;
     uint256 constant ATTO = 10 ** 18;
     uint256 constant Y_MAX = 1_000_000_000_000_000_000_000_000 * ATTO;
     uint256 constant M_MAX = 1_000_000_000_000_000_000_000_000 * 10 ** PRECISION_DECIMALS;
-    uint8 constant MAX_TOLERANCE = 1;
-    uint8 constant TOLERANCE_PRECISION = 10;
+    uint8 constant MAX_TOLERANCE = 5;
+    uint8 constant TOLERANCE_PRECISION = 11;
     uint256 constant TOLERANCE_DEN = 10 ** TOLERANCE_PRECISION;
-    /// tolerance = (MAX_TOLERANCE * 100) / TOLERANCE_PRECISION %;
+    //// tolerance = (MAX_TOLERANCE * 100) / TOLERANCE_PRECISION %;
 
     function setUp() public {
         vm.startPrank(superAdmin);
@@ -55,12 +54,13 @@ contract ProtocolNFTAMMFactoryFuzzTest is TestCommonFoundry, Utils {
     * This test is entirely centered on the math of the Dual Linear NFT AMM.
     */
     function testFuzzNFTAMMCalculatorMath(uint256 mBuy, uint256 bBuy, uint256 mSell, uint256 bSell, uint64 q, bool isSale ) public {
+        /// we make sure that variables are within the bounderies to avoid unintended reverts
         mBuy = mBuy%M_MAX;
         bBuy = bBuy%Y_MAX;
         mSell = mSell%M_MAX;
         bSell = bSell%Y_MAX;
 
-        // we make sure the curves comply with the premises mB > mS, and bB > bS.
+        /// we make sure the curves comply with the premises mB > mS, and bB > bS.
         if(mSell >= mBuy){
             if(mBuy == 0) mBuy = 1;
             mSell = mBuy - 1;
@@ -70,124 +70,113 @@ contract ProtocolNFTAMMFactoryFuzzTest is TestCommonFoundry, Utils {
             bSell = bBuy - 1;
         }
          
+        /// we create the factory
         protocolAMMCalculatorFactory = createProtocolAMMCalculatorFactory();
         
+        /// we create the buy and line curves with the fuzz variables
         LineInput memory buy = LineInput(mBuy, bBuy);
         LineInput memory sell = LineInput(mSell, bSell);
 
+        /// we create the actual calculator with such curves
         ProtocolNFTAMMCalcLinear calc = ProtocolNFTAMMCalcLinear(protocolAMMCalculatorFactory.createDualLinearNFT(buy, sell, address(applicationAppManager)));
 
+        /// the price that will be returned by the calculator
         uint256 price;
+        /// the response from the Python script that will contain the price calculated "offchain"
         bytes memory res;
 
+        /// we carry out the swap depending if it is a sale or a purchase
         if(isSale){
-            if( q < 2) q = 2;
+            /// we make sure q is at least 1 to avoid underflow (only for the sale case)
+            if( q < 1) q = 1;
+            /// we calculate the price through the calculator and store it in *price*
             price = calc.calculateSwap(0, q, 1, 0);
 
-            {
-                string[] memory inputs = new string[](7);
-                inputs[0] = "python3";
-                inputs[1] = "script/python/linear_calculator.py"; 
-                inputs[2] = mSell.toString();
-                inputs[3] = "8"; // decimals
-                inputs[4] = bSell.toString();
-                inputs[5] = uint256(q - 1).toString();
-                inputs[6] = "1"; // y formatted in atto
-                res = vm.ffi(inputs);
-            }
+            /// we then call the Python script to calculate the price "offchain" and store it in *res*
+            string[] memory inputs = _buildFFILinearCalculator(mSell, "8", bSell, q - 1);
+            res = vm.ffi(inputs);
         } 
         else{
-
+            /// we calculate the price through the calculator and store it in *price*
             price = calc.calculateSwap(0, q, 0, 1);
 
-            {
-                string[] memory inputs = new string[](7);
-                inputs[0] = "python3";
-                inputs[1] = "script/python/linear_calculator.py"; 
-                inputs[2] = mBuy.toString();
-                inputs[3] = "8"; // decimals
-                inputs[4] = bBuy.toString();
-                inputs[5] = uint256(q).toString();
-                inputs[6] = "1"; // y formatted in atto
-                res = vm.ffi(inputs);
-            }
+            /// we then call the Python script to calculate the price "offchain" and store it in *res*
+            string[] memory inputs = _buildFFILinearCalculator(mBuy, "8", bBuy, q);
+            res = vm.ffi(inputs); 
         }
-        bytes memory priceBytes = bytes(price.toString());
-
+        
+        /// some debug logging 
         emit Log(res);
-        emit Log(priceBytes);
         emit Price(price);
 
+        /// we determine if the response was returned as a possible ascii or a decimal
         bool isAscii = isPossiblyAnAscii(res);
         emit IsAscii(isAscii);
 
+        /// if it is a possible ascii, then we decode it and start the comparison
         if(isAscii){
-            // we compare if both results are exactly the same
-            if(areBytesEqual(priceBytes, res)){
-                // if they are then we're done here. Test passed.
-                emit Passed();
-                return;
-            // If they are not, then we calculate the difference...
-            }else{
-                uint diff;
-                assembly{
-                    diff := xor(priceBytes, res)
-                }
-                emit Diff(diff);
-                // ... and we see if that difference is within the percentage tolerance
-                if(((uint(diff) * TOLERANCE_DEN)) / price > MAX_TOLERANCE ){
-                    // if it is not, it is possible that what we thought was an ascii, was not.
-                    // so we treat the number as adecimal string that needs to be decoded.
-                    uint pythonPrice = decodeHexDecimalBytes(res);
-                    // now we compare if the decoded number is exactly the same as the price from the calculator
-                    if(price != pythonPrice){
-                        // if they are not, we then proceed to check if the difference is within tolerance
-                        // to avoid underflow, we check which one is greater than the other one
-                        if(price  > pythonPrice){
-                            if(((price - pythonPrice) * TOLERANCE_DEN) / pythonPrice  > MAX_TOLERANCE){
-                                revert OutOfTolerance(price, pythonPrice);
-                            }
-                        }else{
-                            if(((pythonPrice - price) * TOLERANCE_DEN) / price  > MAX_TOLERANCE){
-                                revert OutOfTolerance(price, pythonPrice);
-                            }
-                        }
-                        
-                    }else{
-                        // if they are the same, then we passed the test.
-                        emit Passed();
-                        return;
-                    }
-                }else{
-                    // if the difference is within the tolerance, then we passed the test.
-                    emit Passed();
-                    return;
-                } 
+
+            uint resUint = decodeAsciiUint(res);
+            emit Price(resUint);
+
+            /// we calculate the difference beetween these 2 values
+            uint diff = safeDiff(resUint,price);
+            emit Diff(diff);
+            /// we check if the difference is below the max tolerance percentage
+            if(diff !=0 && (diff * TOLERANCE_DEN) / price > MAX_TOLERANCE ){
+                /// if it is above tolerance, it is possible that we thought *res* was an ascii, but it was not.
+                /// we compare the numbers again, but now as decimals, to see if they are within the tolerance
+                _compareAsDecimals(price, res);
             }
         }
         /// if the response was not an ascii.
         else{
-            // we go from bytes to uint
-            uint pythonPrice= decodeHexDecimalBytes(res);
-            // if the prices are not exactly the same, we see if they are at least within the tolerance
-             if(price != pythonPrice){
-                // to avoid underflow, we check first which one is greater than the other one.
-                if(price  > pythonPrice){
-                    if(((price - pythonPrice) * TOLERANCE_DEN) / pythonPrice  > MAX_TOLERANCE){
-                        revert OutOfTolerance(price, pythonPrice);
-                    }
-                }else{
-                    if(((pythonPrice - price) * TOLERANCE_DEN) / price  > MAX_TOLERANCE){
-                        revert OutOfTolerance(price, pythonPrice);
-                    }
-                }
-                }else{
-                    // if they are, then we passed the test.
-                    emit Passed();
-                    return;
-                }
+            /// we compare the numbers to see if they are within the tolerance
+            _compareAsDecimals(price, res);
         }
         
+        emit Passed();
     }
+
+    /**
+    * @dev creates the input array specifically for the linear_calculator.py script.
+    */
+    function _buildFFILinearCalculator(uint256 m, string memory decimals, uint256 b, uint64 q) internal pure returns(string[] memory) {
+        string[] memory inputs = new string[](7);
+        inputs[0] = "python3";
+        inputs[1] = "script/python/linear_calculator.py"; 
+        inputs[2] = m.toString();
+        inputs[3] = decimals; 
+        inputs[4] = b.toString();
+        inputs[5] = uint256(q).toString();
+        inputs[6] = "1"; /// y formatted in atto
+        return inputs;
+    }
+
+    /**
+    * @dev compares if a uints is similar enough to an encoded uint. 
+    * i.e. The encoded value: 0x23456 is actually decimal 23456 and not hex 0x23456 which is 144470
+    * The tolerance parameters are global to the contract.
+    */
+    function _compareAsDecimals(uint256 price, bytes memory res) internal pure {
+        /// we go from bytes to uint
+        uint pythonPrice= decodeHexDecimalBytes(res);
+        /// we compare the numbers to see if they are within the tolerance
+            _compareWithTolerance(price, pythonPrice);
+    }
+
+    /**
+    * @dev compares if 2 uints are similar enough. The tolerance parameters are global to the contract.
+    */
+    function _compareWithTolerance(uint x, uint y) internal pure {
+        /// to avoid underflow, we check first which one is greater than the other one.
+        uint diff = safeDiff(x,y);
+        /// we calculate difference % as diff/(smaller number).
+        if(diff !=0 && (diff * TOLERANCE_DEN) / ( x > y ? y : x)  > MAX_TOLERANCE){
+            revert OutOfTolerance(x, y);
+        }
+    }
+
+    
 
 }
