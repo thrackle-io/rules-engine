afa384dd22e1708d64643b391e4ebd8737d78a5e
mpetersoCode55 <michael@thrackle.io>
2024-01-25 09:37:28 -0500

    Update deployment-check.yml

---
+++ install-deploy-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/.bashrc
+foundryup
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+
+git submodule update --init --recursive   
+
+forge script script/DeployAllModules.s.sol --ffi --broadcast
+forge script script/clientScripts/ApplicationDeployAll.s.sol --ffi  --broadcast -vvv --non-interactive
