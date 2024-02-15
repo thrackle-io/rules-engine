cd1b065ccf15c33bfdfb57848dab2b90b7190659
Oscar Serna <oscar.eduardo.serna@gmail.com>
2024-02-14 20:09:34 -0600

    improved imports in facets

--- src/client/token/handler/diamond/ERC20TaggedRuleFacet.sol
+++ src/client/token/handler/diamond/ERC20TaggedRuleFacet.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {StorageLib as lib} from "../diamond/StorageLib.sol";
-import "../../../../protocol/economic/IRuleProcessor.sol";
-import {Rule} from "../common/DataStructures.sol";
-import {ActionTypes} from "src/common/ActionEnum.sol";
+import "./FacetsCommonImports.sol";
 import "../common/FacetUtils.sol";
-import "../../../application/IAppManager.sol";
 import "../ruleContracts/HandlerAccountMinMaxTokenBalance.sol";
 import "./TradingRuleFacet.sol";
 
--- src/client/token/handler/diamond/FacetsCommonImports.sol
+++ src/client/token/handler/diamond/FacetsCommonImports.sol
@@ -5,9 +5,8 @@ import {StorageLib as lib} from "../diamond/StorageLib.sol";
 import "../../../../protocol/economic/IRuleProcessor.sol";
 import {Rule} from "../common/DataStructures.sol";
 import {ActionTypes} from "src/common/ActionEnum.sol";
-import "../common/FacetUtils.sol";
 import "../../../application/IAppManager.sol";
-import "../ruleContracts/HandlerAccountMinMaxTokenBalance.sol";
-import "./TradingRuleFacet.sol";
+
+
 
 abstract contract FacetsCommonImports{}
