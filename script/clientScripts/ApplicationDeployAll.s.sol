ef8a8f08ccaeca921e9a80974a4c629ab9e0f385
Oscar Serna <oscar.eduardo.serna@gmail.com>
2024-02-15 16:22:47 -0600

    enabled ApplicationDeployAll verification script

--- script/clientScripts/ApplicationDeployAll.s.sol
+++ script/clientScripts/ApplicationDeployAll.s.sol
@@ -121,12 +121,12 @@ contract ApplicationDeployAllScript is Script, DeployBase {
         vm.stopBroadcast();
 
         // Verify Installation------------------------------------------------------------------
-        // vm.startBroadcast(vm.envUint("QUORRA_PRIVATE_KEY"));
-        // verifyAppManager();
-        // verifyERC20(true);
-        // verifyERC20(false);
-        // verifyERC721();
-        // vm.stopBroadcast();
+        vm.startBroadcast(vm.envUint("QUORRA_PRIVATE_KEY"));
+        verifyAppManager();
+        verifyERC20(true);
+        verifyERC20(false);
+        verifyERC721();
+        vm.stopBroadcast();
         // --------------------------------------------------------------------------------------
     }
 
@@ -189,21 +189,21 @@ contract ApplicationDeployAllScript is Script, DeployBase {
 
     function verifyERC20(bool isCoin1) public view {
        // Make sure the ERC20 Contract has been deployed, if not calling a public function on it will revert.
-       isCoin1 ? coin1.getHandlerAddress() : coin2.getHandlerAddress();
+       isCoin1 ? ERC20HandlerMainFacet(address(coin1)).getHandlerAddress() : ERC20HandlerMainFacet(address(coin2)).getHandlerAddress();
 
        // Make sure the ERC20 Handler Contract has been deployed, if not calling a public function on it will revert.
        isCoin1 ? VersionFacet(address(applicationCoinHandlerDiamond)).version() :  VersionFacet(address(applicationCoinHandlerDiamond2)).version();
 
        // Make sure the ERC20 Pricing Module Contract has been deployed, if not calling a public function on it will revert.
-        exchange.version();
+        VersionFacet(address(exchange)).version();
 
         // Checking to make sure ERC20 has a handler
         if(isCoin1) {
-            if(coin1.getHandlerAddress() != address(applicationCoinHandlerDiamond)) {
+            if( ERC20HandlerMainFacet(address(coin1)).getHandlerAddress() != address(applicationCoinHandlerDiamond)) {
                 revert("Frankenstein Coin not correctly connected to its handler");
             }
         } else {
-            if(coin2.getHandlerAddress() != address(applicationCoinHandlerDiamond2)) {
+            if( ERC20HandlerMainFacet(address(coin2)).getHandlerAddress() != address(applicationCoinHandlerDiamond2)) {
                 revert("Dracula Coin not correctly connected to its handler");
             }
         }
@@ -219,7 +219,7 @@ contract ApplicationDeployAllScript is Script, DeployBase {
         }
 
         // Checking to make sure the ERC20 is registered with the AppManager
-        if(coin1.getAppManagerAddress() != address(applicationAppManager)) {
+        if( ERC20HandlerMainFacet(address(coin1)).getAppManagerAddress() != address(applicationAppManager)) {
             revert("ERC20 not properly registered with the App Manager");
         }
         if(keccak256(abi.encodePacked(applicationAppManager.getTokenID(address(isCoin1 ? coin1 : coin2)))) != keccak256(abi.encodePacked(isCoin1 ? "Frankenstein Coin" : "Dracula Coin"))) {
