f6e46e9a8aec807d19cf4e464d47c2a3c566377c
Robert Kotz <robertkotz@Roberts-MBP.home>
2024-03-12 15:16:57 -1000

    use chain_id from system's env

--- script/ParseProtocolDeploy.sh
+++ script/ParseProtocolDeploy.sh
@@ -2,7 +2,14 @@
 
 ENV_FILE=".env"
 
-CHAIN_ID=31337
+if [[ -n $CHAIN_ID ]]; then
+  CHAIN_ID=${CHAIN_ID}
+elif [[ -n $DB_CHAIN ]]; then
+  CHAIN_ID=${DB_CHAIN}
+else
+  CHAIN_ID=31337
+fi
+
 settingChainID=false
 
   for var in "$@"
