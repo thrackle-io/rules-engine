7915019e3b49279aa6c809fd00d7dbf7f89e4f28
Robert Kotz <robertkotz@Roberts-MBP.home>
2024-05-14 16:51:52 -1000

    testing GHA docker layer caching

--- docker-scripts/compile-tron.sh
+++ docker-scripts/compile-tron.sh
@@ -3,6 +3,8 @@ set -e
 
 source ~/.bashrc
 
+# Just testing GHA layer caching, need a commit to push...
+
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
