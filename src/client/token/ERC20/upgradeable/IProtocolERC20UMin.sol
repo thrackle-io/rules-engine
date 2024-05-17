6337498100087d40f57f0caa20c0bf8ba24bec9a
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2024-05-17 12:47:55 -0500

    Added IProtocolERC20UMin interface

---
+++ src/client/token/ERC20/upgradeable/IProtocolERC20UMin.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @title  Upgradeable ERC20 Protocol Interface Minimal implementation model
+ * @author @ShaneDuncan602, @oscarsernarosero, @TJ-Everett
+ * @notice This is the base contract for all protocol ERC20Upgradeables
+ * @dev Using this interface requires the implementing token properly handle the listed functions as well as insert the checkAllRules hook into _beforeTokenTransfer
+ */
+interface IProtocolERC20UMin {
+    event AD1467_HandlerConnected(address indexed handlerAddress, address indexed assetAddress); 
+    error ZeroAddress();
+
+    /**
+     * @dev this function returns the handler address
+     * @return handlerAddress
+     */
+    function getHandlerAddress() external view returns (address);
+
+    /**
+     * @dev Function to connect Token to previously deployed Handler contract
+     * @param _deployedHandlerAddress address of the currently deployed Handler Address
+     */
+    function connectHandlerToToken(address _deployedHandlerAddress) external;
+}
