ad4d24a5f2b61a5f8e2561806bd722c0cc64e81a
mpetersoCode55 <michael@thrackle.io>
2024-06-14 17:19:00 -0400

    Se 817 create a testnet token deployment of an lp in the amm with the token (#404)
    * Updates to deployment process to better support Pacman
    
    * Update to support non-local deployments
    
    * PR Feedback
    
    ---------
    
    Co-authored-by: Michael Peterson <michaelpeterson@michaels-mbp.myfiosgateway.com>

--- script/clientScripts/Application_Deploy_Example.sh
+++ script/clientScripts/Application_Deploy_Example.sh
@@ -20,11 +20,14 @@ echo Please enter RPC URL
 read ETH_RPC_URL
 echo Please enter the chain id
 read CHAIN_ID
+echo "Is this deployment local? (y or n)"
+read LOCAL
 echo Please enter the gas price
 read GAS_NUMBER
 
-
-sh script/SetupProtocolDeploy.sh --chainid $CHAIN_ID
+if [ "$LOCAL" = "y" ]; then
+  sh script/SetupProtocolDeploy.sh --chainid $CHAIN_ID
+fi
 forge script script/DeployAllModulesPt1.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 sh script/ParseProtocolDeploy.sh --chainid $CHAIN_ID
 forge script script/DeployAllModulesPt2.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
@@ -32,9 +35,39 @@ forge script script/DeployAllModulesPt3.s.sol --ffi --broadcast --rpc-url $ETH_R
 forge script script/DeployAllModulesPt4.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 forge script script/clientScripts/Application_Deploy_01_AppManager.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 sh script/ParseApplicationDeploy.sh 1 --chainid $CHAIN_ID
-forge script script/clientScripts/Application_Deploy_02_ApplicationFT1.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
-sh script/ParseApplicationDeploy.sh 2 --chainid $CHAIN_ID
-forge script script/clientScripts/Application_Deploy_02_ApplicationFT1Pt2.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
+echo "Do you already have an ERC20 deployed (y or n)?"
+read DEPLOYED
+DEPLOYED=$(echo "$DEPLOYED" | tr '[:upper:]' '[:lower:]')  
+while [ "y" != "$DEPLOYED" ] && [ "n" != "$DEPLOYED" ] ; do
+  echo
+  echo "Not a valid answer (y or n)"
+  echo "Do you already have an ERC20 deployed (y or n)?"
+  read DEPLOYED
+  DEPLOYED=$(echo "$DEPLOYED" | tr '[:upper:]' '[:lower:]')  
+done
+if [ "$DEPLOYED" = "y" ]; then
+    echo "Is the deployed ERC20 already protocol enabled (connected to a handler) (y or n)?"
+    read CONNECTED_ALREADY
+    CONNECTED_ALREADY=$(echo "$CONNECTED_ALREADY" | tr '[:upper:]' '[:lower:]')  
+    while [ "y" != "$CONNECTED_ALREADY" ] && [ "n" != "$CONNECTED_ALREADY" ] ; do
+    echo
+    echo "Not a valid answer (y or n)"
+    echo "Is the deployed ERC20 already protocol enabled (connected to a handler) (y or n)?"
+    read DEPLOYED
+    CONNECTED_ALREADY=$(echo "$CONNECTED_ALREADY" | tr '[:upper:]' '[:lower:]')  
+    done
+    if [ "$CONNECTED_ALREADY" = "n" ]; then
+      forge script script/clientScripts/DeployERC20Handler.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
+    fi
+else 
+    forge script script/clientScripts/Application_Deploy_02_ApplicationFT1.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
+    sh script/ParseApplicationDeploy.sh 2 --chainid $CHAIN_ID
+    forge script script/clientScripts/Application_Deploy_02_ApplicationFT1Pt2.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
+fi
+
+forge script script/clientScripts/Application_Deploy_03_ApplicationFT2.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
+sh script/ParseApplicationDeploy.sh 6 --chainid $CHAIN_ID
+forge script script/clientScripts/Application_Deploy_03_ApplicationFT2Pt2.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 forge script script/clientScripts/Application_Deploy_04_ApplicationNFT.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 forge script script/clientScripts/Application_Deploy_04_ApplicationNFTUpgradeable.s.sol --ffi --broadcast --rpc-url $ETH_RPC_URL --gas-price $GAS_NUMBER
 sh script/ParseApplicationDeploy.sh 3 --chainid $CHAIN_ID
