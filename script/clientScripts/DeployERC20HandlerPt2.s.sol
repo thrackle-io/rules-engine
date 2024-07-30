57dd12d3a714159ab040e167bd3849cd745f4ec7
TJ Everett <Taylereverett@gmail.com>
2024-07-30 07:51:33 -0600

    Commit Stashed Changes to Scripts

---
+++ script/clientScripts/DeployERC20HandlerPt2.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import "./DeployBase.s.sol";
+import {HandlerDiamond, HandlerDiamondArgs} from "src/client/token/handler/diamond/HandlerDiamond.sol";
+
+/**
+ * @title Deploy ERC20 Handler Diamond Script
+ * @dev This script will deploy the ERC20 Handler Diamons.
+ */
+contract DeployERC20HandlerPt1 is Script, DeployBase {
+    /// address and private key used to for deployment
+    uint256 privateKey;
+    address ownerAddress;
+    string name;
+    HandlerDiamond applicationCoinHandlerDiamond;
+
+    function run() external {
+        privateKey = vm.envUint("DEPLOYMENT_OWNER_KEY");
+        ownerAddress = vm.envAddress("DEPLOYMENT_OWNER");
+        name = vm.envString("HANDLER_DIAMOND_TO_DEPLOY"); // name of the Diamond
+        vm.startBroadcast(privateKey);
+        ApplicationAppManager applicationAppManager = ApplicationAppManager(vm.envAddress("APPLICATION_APP_MANAGER"));
+        applicationCoinHandlerDiamond = HandlerDiamond(payable(vm.envAddress("APPLICATION_ERC20_HANDLER_ADDRESS")));
+
+        createERC20HandlerDiamondPt2(name, address(applicationCoinHandlerDiamond));
+
+        vm.stopBroadcast();
+    }
+
+}
