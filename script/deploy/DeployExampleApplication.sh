8d059d5e45ac5dee754b60a4b3abcd36cdc9ce86
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2024-08-13 10:56:53 -0500

    Deployed v1.3.1 to Amoy and saved abi

--- script/deploy/DeployExampleApplication.sh
+++ script/deploy/DeployExampleApplication.sh
@@ -36,6 +36,23 @@ echo "################################################################"
 echo
 forge script script/clientScripts/Application_Deploy_02_ApplicationFT1Pt2.s.sol --ffi --broadcast --rpc-url=$ETH_RPC_URL --gas-price $GAS_NUMBER
 
+echo "################################################################"
+echo Deploy the Second ApplicationERC20 and the Handler Diamond 
+echo "################################################################"
+echo
+forge script script/clientScripts/Application_Deploy_03_ApplicationFT2.s.sol --ffi --broadcast --rpc-url=$ETH_RPC_URL --gas-price $GAS_NUMBER
+
+echo "################################################################"
+echo Retrieve the contract info and set environment variables 
+echo "################################################################"
+echo
+bash script/ParseApplicationDeploy.sh 6 --chainid $CHAIN_ID
+
+echo "################################################################"
+echo Deploy the Second ApplicationERC20 and the Handler Diamond pt 2
+echo "################################################################"
+echo
+forge script script/clientScripts/Application_Deploy_03_ApplicationFT2Pt2.s.sol --ffi --broadcast --rpc-url=$ETH_RPC_URL --gas-price $GAS_NUMBER
 
 echo "################################################################"
 echo Deploy the ApplicationERC721 and the Handler Diamond
